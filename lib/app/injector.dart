203c667d4658c6e2b69a10fba7a07d0f1c91ca99
MiqueiasLima <miqueiaslima@outlook.com>
2024-09-29 23:45:21 -0300

    Injecting LocalStore Dependency

--- lib/app/injector.dart
+++ lib/app/injector.dart
@@ -1,3 +1,4 @@
+import 'package:asp_arch/app/data/repositories/localstore_todo_repository.dart';
 import 'package:asp_arch/app/data/repositories/shared_todo_repository.dart';
 import 'package:asp_arch/app/interactor/repositories/todo_repository.dart';
 import 'package:auto_injector/auto_injector.dart';
@@ -6,6 +7,7 @@ final injector = AutoInjector();
 
 void registerInstances(){
   
-  injector.add<TodoRepository>(SharedTodoRepository.new);
+  //injector.add<TodoRepository>(SharedTodoRepository.new);
+  injector.add<TodoRepository>(LocalStoreTodoRepository.new);
 
 }
